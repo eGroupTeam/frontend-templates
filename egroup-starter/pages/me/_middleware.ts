a67bffb88b2a30f6d869c59825fcd2ff2672c3fb
SP_Penguin <abrcdf1023@gmail.com>
2022-04-12 15:46:55 +0800

    fixed middleware lint warning

--- egroup-starter/pages/me/_middleware.ts
+++ egroup-starter/pages/me/_middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
-import type { NextFetchEvent, NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export function middleware(req: NextRequest) {
   const hasLogin = req.cookies.hasLoginCookie;
   if (hasLogin) {
     return NextResponse.next();
